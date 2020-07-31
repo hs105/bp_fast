62a527e5c87d012cc48f6f13bc53bcf34b96185c
hengshuai yao <hengshuai@gmail.com>
2020-07-31 17:46:05 -0600

    code for ill-conditioned; layer 1 being fixed.

--- gms_lin.py
+++ gms_lin.py
@@ -9,7 +9,7 @@ def gen_ill_cond_matrix(xdim = 6, ydim=10):
     A = np.random.rand(ydim, xdim)
     u, s, v = np.linalg.svd(A, full_matrices=False)
     s[0] *= 1e5
-    B = np.dot(u, np.dot(np.diag(s), v))
+    B = np.matmul(u, np.matmul(np.diag(s), v))
     return B.T
     # print(np.allclose(A, Atrue)
 
@@ -28,18 +28,29 @@ def row_wise_dot(a, b):
 
 def gen_data(W, nsamples, xdim):
     X = np.random.randn(nsamples, xdim)
-    Y = X.dot(W)
+    Y = np.matmul(X, W)
     return X, Y
+
+def rate_of_mr(A):
+    mu = min(np.linalg.eigvals(A + A.T)) / 2
+    sigma = np.linalg.norm(A, 2)
+    return np.sqrt(1 - (mu / sigma) ** 2)
 
 def last_layer_1step(X, Y, w):
     '''
-    our algorithm for updating the last layer ( only one step).
+    our algorithm for updating the last layer (only one step).
     '''
-    A = X.T.dot(X)
-    b = X.T.dot(Y)
+
+    A = np.matmul(X.T, X)
+    rate = rate_of_mr(A)
+    # print('cond(A)=', np.linalg.cond(A))
+    # print('norm(A)=', np.linalg.norm(A))
+    b = np.matmul(X.T, Y)
 
     R = A.dot(w) - b
     P = A.dot(R)
+
+    err_Y = X.dot(w) - Y
 
     alpha_n = row_wise_dot(P.T, R.T)
     alpha_d = row_wise_dot(P.T, P.T)
@@ -52,25 +63,28 @@ def last_layer_1step(X, Y, w):
     #recalculate; R1 should always be smaller than R
     R1 = A.dot(w) - b
 
-    return w, alpha, R, R1
+    return w, alpha, R, R1, err_Y, rate
 
 def ill_example1(W, num_steps):
     '''this illustrate our algorithm for solving an ill-conditioned learning example with only one layer '''
     m, n = (xdim, ydim)
     w = np.random.rand(m, n)
-    final_acc = None
     st = np.empty((num_steps, w.shape[1]))
     err = []
     err1 = []
+    err_Y = []
+    rates = []
     for k in range(num_steps):
         X, Y = gen_data(W, nsamples, xdim)
 
-        w, alpha, R, R1 = last_layer_1step(X, Y, w)
+        w, alpha, R, R1, errY, rate = last_layer_1step(X, Y, w)
 
         st[k, :] = alpha
         err.append(np.linalg.norm(R))
         err1.append(np.linalg.norm(R1))
-    return w, st, err, err1, final_acc
+        err_Y.append(np.linalg.norm(errY))
+        rates.append(rate)
+    return w, st, err, err1, err_Y, rates
 
 def min_delta(W, num_steps):
     '''minimizing the prediction error'''
@@ -91,16 +105,17 @@ def min_delta(W, num_steps):
 
 # min_delta(W, num_steps=100)
 
-w, st, err, err1, final_acc = ill_example1(W, num_steps=100)
+w, st, err, err1, err_Y, rates = ill_example1(W, num_steps=100)
 plt.figure(1)
 plt.plot(err, '-b+', label='BEFORE weight update')
 plt.plot(err1, '-k.', label='AFTER weight update')
+plt.plot(err_Y, '--ro', label='error of predicting Y')
+plt.plot(rates, '-g<', label='rates')
 plt.legend()
 plt.xlabel('steps')
 plt.ylabel('loss (squared norm of gradient)')
 plt.yscale('log')
 plt.title('the \'AFTER\' curve should always be smaller')
-plt.show()
 
 
 plt.figure(2)
@@ -110,6 +125,128 @@ for i in range(len(markers)):#range(st.shape[1]):
 plt.xlabel('steps')
 plt.ylabel('step-sizes')
 # plt.yscale('log')
+
+#similar ill-conditioned experiment 2: two-layer linear
+#for the same ill-conditioned example
+#use W2W1X to approximate Y
+
+def last_layer_1step2(X, Y, w1, w2):
+    '''
+    our algorithm for updating the last layer (only one step), w2
+    w1 is fixed for now
+    '''
+    print('cond(X.T*X):', np.linalg.cond(np.matmul(X.T, X)))
+    X = X.dot(w1)
+    XTX = np.matmul(X.T, X)
+    print('cond(w1.T*X.T*X*w1):', np.linalg.cond(XTX))
+    _, sg, _ = np.linalg.svd(XTX)
+    #looks the smaller matrix, k by k conditioning is important.
+    print('eig(w1.T*X.T*X*w1):, top-k:', sg[:xdim], ', remaining: ', sg[xdim:])
+
+    w2, alpha, R, R1, err_Y, rate = last_layer_1step(X, Y, w2)
+    return w2, alpha, R, R1, err_Y, rate
+
+
+def ill_example1_2layers(W, num_steps):
+    '''this illustrate our algorithm for solving the same ill-conditioned learning example but with two layers '''
+    m, n = (xdim, ydim)
+    w1 = np.random.rand(m, m)
+    #w1 = np.eye(m) + 0.1 * np.random.rand(m, m)
+    # print('cond(w1)=', np.linalg.cond(w1))
+    w2 = np.random.rand(m, n)
+    st = np.empty((num_steps, w.shape[1]))
+    err = []
+    err1 = []
+    err_Y = []
+    rates = []
+    for k in range(num_steps):
+        X, Y = gen_data(W, nsamples, xdim)
+
+        w2, alpha, R, R1, errY, rate = last_layer_1step2(X, Y, w1, w2)
+
+        st[k, :] = alpha
+        err.append(np.linalg.norm(R))
+        err1.append(np.linalg.norm(R1))
+        err_Y.append(np.linalg.norm(errY))
+        rates.append(rate)
+    return w, st, err, err1, err_Y, rates
+
+
+# w, st, err, err1, err_Y, rates = ill_example1_2layers(W, num_steps=100)
+# plt.figure(3)
+# plt.plot(err, '-b+', label='2-layer w2: BEFORE weight update')
+# plt.plot(err1, '-k.', label='2-layer w2: AFTER weight update')
+# plt.plot(err_Y, '-ro', label='2-layer w2: error of predicting Y')
+# plt.plot(rates, '-g<', label='rates')
+# plt.legend()
+# plt.xlabel('steps')
+# plt.ylabel('loss (squared norm of gradient)')
+# plt.yscale('log')
+# plt.title('two-layer linear system: the \'AFTER\' curve should always be smaller')
+#
+# plt.figure(4)
+# markers = ['-b+', '--ko', ':g<', '--rs']
+# for i in range(len(markers)):#range(st.shape[1]):
+#     plt.plot(st[:, i], markers[i])
+# plt.xlabel('steps')
+# plt.ylabel('step-sizes')
+#
+# plt.show()
+
+def ill_example1_2layers_1(W, n1, num_steps):
+    ''' n1: output size of layer 1
+    n1 is smaller or larger than m
+    '''
+    m, n = (xdim, ydim)
+    if m > n1:
+        w1 = np.eye(n1) + 0.1 * np.random.rand(n1, n1)
+        #w1 = np.vstack((w1, np.zeros((m-n1, n1))))
+        w1 = np.vstack((w1, 0.1 * np.random.rand(m-n1, n1)))
+    elif m < n1:
+        w1 = np.eye(m) + 0.9 * np.random.rand(m, m)
+        w1 = np.hstack((w1, 0.1 * np.random.rand(m, n1-m)))
+    else:
+        w1 = np.eye(m) + 0.1 * np.random.rand(m, m)
+
+    assert w1.shape == (m, n1)
+    print('cond(w1)=', np.linalg.cond(w1))
+    w2 = np.random.rand(n1, n)
+    st = np.empty((num_steps, w.shape[1]))
+    err = []
+    err1 = []
+    err_Y = []
+    rates = []
+    for k in range(num_steps):
+        X, Y = gen_data(W, nsamples, xdim)
+
+        w2, alpha, R, R1, errY, rate = last_layer_1step2(X, Y, w1, w2)
+
+        st[k, :] = alpha
+        err.append(np.linalg.norm(R))
+        err1.append(np.linalg.norm(R1))
+        err_Y.append(np.linalg.norm(errY))
+        rates.append(rate)
+    return w2, st, err, err1, err_Y, rates
+
+# n1 = xdim - 2 #condition number being small, but the error is high because of compressing information.
+n1 = xdim + 2 #overdetermined. the underlying matrix is not invertible, so matrix is ill-conditioned. --confirm?
+w, st, err, err1, err_Y, rates = ill_example1_2layers_1(W, n1, num_steps=100)
+
+plt.figure(3)
+plt.plot(err, '-b+', label='2-layer w2: BEFORE weight update')
+plt.plot(err1, '-k.', label='2-layer w2: AFTER weight update')
+plt.plot(err_Y, '-ro', label='2-layer w2: error of predicting Y')
+plt.plot(rates, '-g<', label='rates')
+plt.legend()
+plt.xlabel('steps')
+plt.ylabel('loss (squared norm of gradient)')
+plt.yscale('log')
+plt.title('two-layer linear system: the \'AFTER\' curve should always be smaller')
+
+plt.figure(4)
+markers = ['-b+', '--ko', ':g<', '--rs']
+for i in range(len(markers)):#range(st.shape[1]):
+    plt.plot(st[:, i], markers[i])
+plt.xlabel('steps')
+plt.ylabel('step-sizes')
 plt.show()
-
-
